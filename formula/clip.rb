8489a5c6051a574fad4d1f4fb456e04b27e2a4e5
mingrammer <mingrammer@gmail.com>
2018-03-17 13:16:18 +0900

    Brew formula update for clip version v0.0.2

--- formula/clip.rb
+++ formula/clip.rb
@@ -1,9 +1,9 @@
 class Clip < Formula
   desc "A simple key-value store for clipboard"
   homepage "https://github.com/mingrammer/clip"
-  url "https://github.com/mingrammer/clip/releases/download/v0.0.1/clip_0.0.1_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "c501d929db4db9bde80de32076b7c96271d9861f678a881e8cc98704ce8cc182"
+  url "https://github.com/mingrammer/clip/releases/download/v0.0.2/clip_0.0.2_darwin_amd64.tar.gz"
+  version "0.0.2"
+  sha256 "c9de3b649edbb1580802391f9ac54b1912589df99e2eb53d15cdc5cefad90a40"
 
   def install
     bin.install "clip"
